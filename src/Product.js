842e14936a0a620271c75cea0599e48b2a3195bf
Saijeet7 <np03s200062@heraldcollege.edu.np>
2022-01-30 21:24:17 +0545

    Bug 1 solved

---
+++ src/Product.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Product.css"
+function Product() {
+  return (
+    <div className="product">
+        <div className="product__info">
+            <p>The product information</p>
+            <p className="product__price">
+                <small>$</small>
+                <strong>19.99</strong>
+            </p>
+            <div className="product__rating">
+                <p>*</p>
+            </div>
+
+            <img src="https://m.media-amazon.com/images/I/51T-sMqSMiL._AC_SY780_.jpg" alt="" />
+            <button>Add to Basket</button>
+        </div>
+    </div>
+  )
+}
+
+export default Product
