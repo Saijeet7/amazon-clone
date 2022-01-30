1fadc20d95ea4e78a4cab22ea017ba78da5a8c2a
Saijeet7 <np03s200062@heraldcollege.edu.np>
2022-01-30 22:08:53 +0545

    Home page complete

--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import "./Product.css"
-function Product() {
+function Product({title, image, price, rating}) {
   return (
     <div className="product">
         <div className="product__info">
-            <p>The product information</p>
+            <p>{title}</p>
             <p className="product__price">
                 <small>$</small>
-                <strong>19.99</strong>
+                <strong>{price}</strong>
             </p>
             <div className="product__rating">
-                <p>*</p>
+                {Array(rating)
+                    .fill()
+                    .map((_,i)=>(
+                 <p>⭐</p>
+                    ))}
             </div>
         </div>
-        <img src="https://m.media-amazon.com/images/I/51T-sMqSMiL._AC_SY780_.jpg" alt="" />
+        <img src={image} alt="" />
         <button>Add to Basket</button>
     </div>
   )
