5f99c66fea61beb822227991e1791d4deec40ef4
Saijeet7 <np03s200062@heraldcollege.edu.np>
2022-02-01 22:52:09 +0545

    User Authentication Complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
+import { auth} from "./firebase"
+import { useStateValue} from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() =>{
+    //will run only once when the app component loads...
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('The User Is >>> ',authUser);
+
+      if (authUser){
+        //the user just logged in / the user was not logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else{
+        //the user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
   return (
     //BEM Convention
     <Router>
--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,16 @@ import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import {Link} from "react-router-dom";
 import {useStateValue} from "./StateProvider";
+import {auth} from "./firebase"
 
 function Header() {
-  const [{basket}, dispatch] = useStateValue();
+  const [{basket, user}, dispatch] = useStateValue();
+
+  const handleAuthentication = () =>{
+    if (user){
+      auth.signOut();
+    }
+  }
 
   return (
     <div className="header">
@@ -18,10 +25,12 @@ function Header() {
           <SearchIcon className="header_searchIcon"/>
       </div>
       <div className="header__nav">
-        <div className="header_option">
-            <span className='header__optionLineOne'>Hello Guest</span>
-            <span className='header__optionLineTwo'>Sign In</span>
-        </div>
+        <Link to={!user &&'/login'}>
+          <div onClick={handleAuthentication} className="header_option">
+              <span className='header__optionLineOne'>Hello Guest</span>
+              <span className='header__optionLineTwo'>{user ? 'Sign Out': 'Sign In'}</span>
+          </div>
+        </Link>
         <div className="header_option">
             <span className='header__optionLineOne'>Returns</span>
             <span className='header__optionLineTwo'>& Orders</span>        
