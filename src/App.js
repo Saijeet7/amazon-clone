2e12c383c5602d0b3cdb980831147acd40360da6
Saijeet7 <np03s200062@heraldcollege.edu.np>
2022-02-01 11:00:20 +0545

    Bug 3 Route fixed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 
@@ -11,12 +12,14 @@ function App() {
     //BEM Convention
     <Router>
       <div className="app">
-        <Header/>
         <Routes>
-          <Route exact path="/checkout" element={<Checkout/>}/> 
+          <Route exact path="/login" element={<Login/>}/>
         </Routes>
         <Routes>
-          <Route exact path="/" element={<Home/>}/> 
+          <Route exact path="/checkout" element={[<Header/>,<Checkout/>]}/> 
+        </Routes>
+        <Routes>
+          <Route exact path="/" element={[<Header/>,<Home/>]}/> 
         </Routes>
       </div>
     </Router>
---
+++ src/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+  return (
+  <div>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'/>
+  </div>);
+}
+
+export default Login;
